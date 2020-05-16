4bf06084bba408dc09c92a8d5223c71711339f5b
Yash Narang <f20180150@goa.bits-pilani.ac.in>
2020-05-16 22:08:56 +0530

    scrollbar added

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import ttk
 from PIL import Image,ImageTk
 import os
@@ -14,53 +15,79 @@ class Table:
             for j in range(3): 
                   
                 self.e = Entry(root, width=20, fg='blue', 
-                               font=('Arial',16,'bold')) 
+                               font=('Arial',10,'bold')) 
                   
                 self.e.grid(row=i, column=j) 
                 self.e.insert(END, data[i][j]) 
 
 def mycmp(s1, s2):
-	(a,b,c)=s1
-	(x,y,z)=s2
-	f=c[1:]
-	h=z[1:]
-	f=str(f)+str(c[0])
-	h=str(h)+str(z[0])
-	return f>=h
+    (a,b,c)=s1
+    (x,y,z)=s2
+    f=c[1:]
+    h=z[1:]
+    f=str(f)+str(c[0])
+    h=str(h)+str(z[0])
+    #print(f,h)
+    return f>=h
+
+def populate(frame,data):
+    '''Put in some fake data'''
+    for i in range(len(data)): 
+        for j in range(3):
+            e = Entry(frame, width=20, fg='blue',font=('Arial',16,'bold')) 
+            e.grid(row=i, column=j)
+            e.insert(END, data[i][j]) 
+
+
+def onFrameConfigure(canvas):
+    '''Reset the scroll region to encompass the inner frame'''
+    canvas.configure(scrollregion=canvas.bbox("all"))
 
 def show():
-	top=Toplevel()
-	top.geometry("700x400") 
-	top.title('List of Section')
-	text=clicked.get()
+    text=clicked.get()
 
-	data = set()
+    data = set()
 
-	for i in range(df.shape[0]):
-		x=df['Section'][i].split('-')
-		if(x[0]==text):
-			y=(df['StudentID'][i],df['StudentName'][i],x[1])
-			data.add(y)
-	data=list(data)
-	
-	sorted(data,key=functools.cmp_to_key(mycmp))
+    for i in range(df.shape[0]):
+        x=df['Section'][i].split('-')
+        if(x[0]==text):
+            y=(df['StudentID'][i],df['StudentName'][i],x[1])
+            data.add(y)
+    data=list(data)
 
-	y=("","","")
-	z=set()
-	for i in range(0,len(data)-1):
-		if(data[i][2][1:]!=data[i+1][2][1:]):
-			z.add(i+1)
-	c=0;
-	for i in z:
-		data.insert(c+int(i),y)
-		c=c+1;
-	y=('ID','NAME','SECTION')
-	data.insert(0,y)
-	y=("","","")
-	data.insert(1,y)
+    sorted(data,key=functools.cmp_to_key(mycmp))
 
-	t=Table(top,data)
+    y=("","","")
+    z=set()
+    for i in range(0,len(data)-1):
+        #print(data[i][2][1]!=data[i+1][2][1],data[i][2][1],data[i+1][2][1])
+        if(data[i][2][1:]!=data[i+1][2][1:] or data[i][2][0]!=data[i+1][2][0]):
+            z.add(i+1)
+    c=0;
+    for i in z:
+        data.insert(c+int(i),y)
+        c=c+1;
+    y=('      ID','       NAME','       SECTION')
+    data.insert(0,y)
+    y=("","","")
+    data.insert(1,y)
 
+    root = tk.Tk()
+    root.geometry("700x400") 
+    canvas = tk.Canvas(root, borderwidth=0, background="#ffffff")
+    frame = tk.Frame(canvas, background="#ffffff")
+    vsb = tk.Scrollbar(root, orient="vertical", command=canvas.yview)
+    canvas.configure(yscrollcommand=vsb.set)
+
+    vsb.pack(side="right", fill="y")
+    canvas.pack(side="left", fill="both", expand=True)
+    canvas.create_window((4,4), window=frame, anchor="nw")
+
+    frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+
+    populate(frame,data)
+
+    root.mainloop() 
 
 
 p_dir = os.getcwd()
@@ -68,8 +95,8 @@ df=pd.read_excel('Course_List.xls')
 course=set()
 
 for i in range(df.shape[0]):
-	x=df['Section'][i].split('-')
-	course.add(x[0])
+    x=df['Section'][i].split('-')
+    course.add(x[0])
 
 course=list(course)
 
@@ -96,10 +123,11 @@ course.sort()
 drop = ttk.Combobox(root, textvariable=clicked, values=course)
 drop.pack()
 
-myButton = Button(root, text="Show Result", command=show,pady=10)
+myButton = Button(root, text="Show Result", command=show,pady=20)
 myButton.pack()
 
-button_quit = Button(root, text='Exit Program', command=root.quit,pady=10)
-button_quit.pack()
 
 root.mainloop()
+
+
+
