2fc17cbbb921d77428d4a3623776846d7461163b
Yash Narang <f20180150@goa.bits-pilani.ac.in>
2020-05-17 12:12:08 +0530

    spaces added

--- test.py
+++ test.py
@@ -11,14 +11,17 @@ class Table:
     def __init__(self,root,data): 
           
         # code for creating table 
-        for i in range(len(data)): 
+       
+        for i in range(len(data)):
+            
             for j in range(3): 
                   
-                self.e = Entry(root, width=20, fg='blue', 
+                self.e = Entry(root, width=20, fg='black', 
                                font=('Arial',10,'bold')) 
                   
-                self.e.grid(row=i, column=j) 
-                self.e.insert(END, data[i][j]) 
+                self.e.grid(row=i, column=j)
+                if(j==2): 
+                    self.e.insert(END, data[i][j]) 
 
 def mycmp(s1, s2):
     (a,b,c)=s1
@@ -27,16 +30,29 @@ def mycmp(s1, s2):
     h=z[1:]
     f=str(f)+str(c[0])
     h=str(h)+str(z[0])
-    #print(f,h)
-    return f>=h
+    print(f,h)
+    return f>h
 
 def populate(frame,data):
     '''Put in some fake data'''
-    for i in range(len(data)): 
+    for i in range(len(data)):
+        a=data[i][0]
+        b=data[i][1]
+        c=data[i][2]
+        l=[b,c,a]
         for j in range(3):
-            e = Entry(frame, width=20, fg='blue',font=('Arial',16,'bold')) 
+            e = Entry(frame, width=20, fg='black',font=('Arial',16,'bold')) 
             e.grid(row=i, column=j)
-            e.insert(END, data[i][j]) 
+            if(j==2): 
+                x=len(a)
+                y=""
+                if(x!=0):
+                    y=a[x-1]+a[0:x-1]
+                if(x>8):
+                    y='       SECTION'
+                e.insert(END, y)
+            else: 
+                e.insert(END,l[j]) 
 
 
 def onFrameConfigure(canvas):
@@ -50,24 +66,46 @@ def show():
 
     for i in range(df.shape[0]):
         x=df['Section'][i].split('-')
+        y=x[1][1:]
+        y=y+x[1][0]
         if(x[0]==text):
-            y=(df['StudentID'][i],df['StudentName'][i],x[1])
-            data.add(y)
+            z=(y,df['StudentID'][i],df['StudentName'][i])
+            data.add(z)
     data=list(data)
-
-    sorted(data,key=functools.cmp_to_key(mycmp))
+    data.sort()
+    #sorted(data,key=functools.cmp_to_key(mycmp))
 
     y=("","","")
     z=set()
-    for i in range(0,len(data)-1):
-        #print(data[i][2][1]!=data[i+1][2][1],data[i][2][1],data[i+1][2][1])
-        if(data[i][2][1:]!=data[i+1][2][1:] or data[i][2][0]!=data[i+1][2][0]):
-            z.add(i+1)
+    i=0
+    while(i<len(data)-1):
+        #print(data[i][0]!=data[i+1][0])
+        if(data[i][0]!=data[i+1][0]):
+            data.insert(int(i+1),y)
+            i=i+2
+            #print(data[i][0],data[i+1])
+            z.add(i)
+        else:
+        	i=i+1
+
     c=0;
-    for i in z:
-        data.insert(c+int(i),y)
-        c=c+1;
-    y=('      ID','       NAME','       SECTION')
+    # data2=set()
+    # for i in data:
+    #     (x,q,w)=i;
+    #     # a=x[0]
+    #     # b=x[1:]+a
+    #     # x=b
+    #     data2.add((q,w,x))
+    # data=list(data2)
+    # for i in z:
+    #     print(i)
+    #     if(c==0):
+    #     	c=0
+    #     data.insert(c+int(i)+1,y)
+    #     if(c==-1):
+    #     	c=0
+    #     c=c+1;
+    y=('       SECTION','      ID','       NAME')
     data.insert(0,y)
     y=("","","")
     data.insert(1,y)
